86016cdfea199837698faedb67f254b0e91eb553
Anna O <2tvv21@gmail.com>
2024-02-10 19:24:31 +0200

    init

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import GetApp from "@/components/GetApp";
+import Camp from "@/components/Camp";
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import Guide from "@/components/Guide";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
+  );
+}
